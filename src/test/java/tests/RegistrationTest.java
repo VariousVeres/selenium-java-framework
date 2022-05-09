4f2295adffdd553642bd0bd9098927d800ec7f8c
VariousVeres <email>
2022-05-09 14:25:19 +0300

    API lugin unsuccesfull

--- src/test/java/tests/RegistrationTest.java
+++ src/test/java/tests/RegistrationTest.java
@@ -1,8 +1,11 @@
 package tests;
 
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -55,9 +58,24 @@ public class RegistrationTest {
         assertThat("Wrong search page url", driver.getCurrentUrl(), equalTo("http://localhost:3000/#/search"));
     }
 
-    @BeforeSuite
-    void login() {
+    public void registrationAndLogInAPI() {
+        driver.get("http://localhost:3000");
+        String email = Helper.getAlphanumericStringWithLength(7) + "@gmail.com";
+        String password = Helper.getAlphanumericStringWithLength(8);
+        Response regResponse = given().when().body("{\"email\":\"" + email + "\",\"password\":\"" + password + "\",\"passwordRepeat\":\"" + password + "\",\"securityQuestion\":{\"id\":1,\"question\":\"Your eldest siblings middle name?\",\"createdAt\":\"2022-05-06T15:10:09.398Z\",\"updatedAt\":\"2022-05-06T15:10:09.398Z\"},\"securityAnswer\":\"asd\"}")
+                .post(Helper.getProperty("host.url") + "/api/Users")
+                .then().statusCode(HttpStatus.SC_CREATED).extract().response();
+        LOGGER.info("Registration response: \n " + regResponse.asString());
 
+        int userId = regResponse.path("data.id");
+        Response secQuestionResponse =given().when().body("{\"UserId\":" + userId + ",\"answer\":\"asd\",\"SecurityQuestionId\":1}")
+                .post(Helper.getProperty("host.url") + "/api/SecurityAnswers/")
+                .then().statusCode(HttpStatus.SC_CREATED).extract().response();
+        LOGGER.info("SecurityAnswers \n: " + secQuestionResponse.asString());
+
+        Response loginResponse = given().when().body("{\"email\":\"" + email + "\",\"password\":\"" + password + "\"}").post(Helper.getProperty("host.url") + "/rest/user/login")
+                .then().extract().response();
+        LOGGER.info("Token \n: " + loginResponse);
     }
 
     @AfterSuite
