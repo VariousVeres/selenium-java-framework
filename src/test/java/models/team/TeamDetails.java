bcf100ef4cbd5053375f3ceba4c61679ca779b08
oleksandr.veresiuk <oleksandr.veresiuk@eleks.com>
2026-01-29 21:24:01 +0200

    TeamDetails comment description

--- src/test/java/models/team/TeamDetails.java
+++ src/test/java/models/team/TeamDetails.java
@@ -1,5 +1,19 @@
 package models.team;
 
+/**
+ * Detailed Team representation returned by GET /teams/{id}.
+ *
+ * Extends {@link BaseTeam} with additional fields available
+ * only in the details endpoint.
+ *
+ * Overrides group getter/setter to use {@link TeamDetailsGroup},
+ * which contains extended group information
+ * (real_devices, virtual_machines).
+ *
+ * Jackson uses the overridden setter to deserialize the "group"
+ * field into {@link TeamDetailsGroup}.
+ */
+
 public class TeamDetails extends BaseTeam {
 
     private String description;
